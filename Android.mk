6687cbb569e0043d9d1d25a5dccc9555bfdee4d3
Steve Kondik <steve@cyngn.com>
2015-10-16 06:52:14 -0700

    vold: Add linkage for fs_mgr changes
    Change-Id: Id6623ee558a43600e474ef89f318574bdc6ba25f

--- Android.mk
+++ Android.mk
@@ -58,6 +58,7 @@ common_shared_libraries := \
 	libsoftkeymaster \
 	libbase \
 	libkeymaster_messages \
+	libext2_blkid
 
 common_static_libraries := \
 	libbootloader_message \
@@ -68,6 +69,8 @@ common_static_libraries := \
 	libscrypt_static \
 	libmincrypt \
 	libbatteryservice \
+	libext2_blkid \
+	libext2_uuid_static
 
 vold_conlyflags := -std=c11
 vold_cflags := -Werror -Wall -Wno-missing-field-initializers -Wno-unused-variable -Wno-unused-parameter
